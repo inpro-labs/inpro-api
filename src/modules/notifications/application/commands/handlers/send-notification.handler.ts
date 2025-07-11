c846f1a1e7391e8e071f41c410eb780378c4ebee
Maxwell Macedo <maxwellmacedo2015@gmail.com>
2025-07-11 01:07:46 -0300

    refactor: implement ValidateNotificationTemplateService to centralize template validation logic and enhance error handling in notification processing

--- src/modules/notifications/application/commands/handlers/send-notification.handler.ts
+++ src/modules/notifications/application/commands/handlers/send-notification.handler.ts
@@ -3,13 +3,12 @@ import { CommandHandler, EventBus, ICommandHandler } from '@nestjs/cqrs';
 import { SendNotificationCommand } from '../send-notification.command';
 import { SendNotificationOutputDTO } from '../../ports/in/send-notification.port';
 import { Err, ID, Ok } from '@inpro-labs/core';
-import { NotificationChannel } from '@modules/notifications/domain/enums/notification-channel.enum';
 import { NotificationStatus } from '@modules/notifications/domain/enums/notification-status.enum';
 import { BusinessException } from '@shared/exceptions/business.exception';
-import { TemplateManagerService } from '@modules/notifications/infra/services/template-manager.service';
 import { INotificationRepository } from '@modules/notifications/domain/interfaces/repositories/notification.repository';
 import { QueueNotificationEvent } from '@modules/notifications/domain/events/queue-notification.event';
 import { NotificationFactory } from '@modules/notifications/infra/factories/notification.factory';
+import { ValidateNotificationTemplateService } from '../../services/validate-notification-template.service';
 
 @Injectable()
 @CommandHandler(SendNotificationCommand)
@@ -17,9 +16,9 @@ export class SendNotificationHandler
   implements ICommandHandler<SendNotificationCommand, SendNotificationOutputDTO>
 {
   constructor(
-    private readonly templateManagerService: TemplateManagerService,
     private readonly notificationRepository: INotificationRepository,
     private readonly eventBus: EventBus,
+    private readonly validateNotificationTemplateService: ValidateNotificationTemplateService,
   ) {}
 
   async execute(
@@ -28,44 +27,19 @@ export class SendNotificationHandler
     const { userId, templateId, templateVariables, channel, channelData } =
       command.dto;
 
-    if (!Object.values(NotificationChannel).includes(channel)) {
-      return Err(
-        new BusinessException(
-          `Invalid channel: ${channel}`,
-          'INVALID_NOTIFICATION_CHANNEL',
-          HttpStatus.BAD_REQUEST,
-        ),
-      );
-    }
-
-    const templateResult = this.templateManagerService.getTemplate(templateId);
+    const templateResult = this.validateNotificationTemplateService.execute(
+      templateId,
+      channel,
+      templateVariables,
+    );
 
     if (templateResult.isErr()) {
-      return Err(
-        new BusinessException(
-          'Template not found',
-          'NOTIFICATION_TEMPLATE_NOT_FOUND',
-          HttpStatus.NOT_FOUND,
-        ),
-      );
+      return Err(templateResult.getErr()!);
     }
 
     const template = templateResult.unwrap();
-
-    if (!template.isChannelAvailable(channel)) {
-      return Err(
-        new BusinessException(
-          `Channel-${channel} not available for template ${template.get('name')}`,
-          'NOTIFICATION_CHANNEL_NOT_AVAILABLE',
-          HttpStatus.BAD_REQUEST,
-        ),
-      );
-    }
-
     const redactedTemplateVariables = template.redactData(channel);
-
     const notificationId = ID.create().unwrap();
-
     const notificationResult = NotificationFactory.make(
       {
         _id: notificationId.value(),
@@ -94,20 +68,6 @@ export class SendNotificationHandler
     }
 
     const notification = notificationResult.unwrap();
-
-    const isVariablesValid = notification
-      .get('template')
-      .validateData(channel, templateVariables);
-
-    if (isVariablesValid.isErr()) {
-      return Err(
-        new BusinessException(
-          isVariablesValid.getErr()!.message,
-          'INVALID_TEMPLATE_VARIABLES',
-          HttpStatus.BAD_REQUEST,
-        ),
-      );
-    }
 
     await this.notificationRepository.save(notification);
 
